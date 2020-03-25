eecc08b6fab3050fe91ea0b2f16f66d7c5fe13cc
SUMIT <sumitojha3292@gmail.com>
2020-03-25 10:19:43 +0530

    Program to print elements of 2D Array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* CPP program to search element in a array using binary search*/ 
+/* CPP program to to print all elements of 2D array*/ 
 
 #include <stdio.h>
 #include <iostream>
@@ -6,37 +6,14 @@ using namespace std;
 
 int main()
 {
-    int key=6,i=0;
-	int start,end,mid;
-    
-    int arr[12]={2,4,6,8,10,12,14,16,18,20,22,24};
-		start=0;
-		end=11;
-    for(i=0;i<12;i++)
-     {
-		 mid=(start+end)/2;
-		 if(key==arr[mid])
-		 {
-			 cout<<"Element is present at "<< mid+1<<"th position"<<endl;
-			 break;
-			 
-			 //mid=start
-		 }
-		 else
-		 {
-			 if(key>arr[mid])
-			 {
-				 start=mid;
-			 }
-			 else
-			 {
-				end=mid; 
-			 }
-			 
-		 }
-	  
-     }	  
-
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			cout<<"("<<i<<","<<j<<")";
+		}
+		cout<<endl;
+	}
 
     return 0;
 }
