945a42959b0ac8c68b5810239c6eb0e03138f7f0
SUMIT <sumitojha3292@gmail.com>
2020-03-25 12:50:24 +0530

    Taking user I/P and print 2D array using for each loop

--- main.cpp
+++ main.cpp
@@ -6,15 +6,25 @@ using namespace std;
 
 int main()
 {
-int A[2][3]={{2,5,9},{7,3,6}};
-int B[2][3]={{6,3,4},{9,5,2}};
-int C[2][3];
-	for(int i=0;i<2;i++)
+	int A[2][3];
+
+	cout<<"Enter the elements of array of 2*3 dimension"<<endl;
+
+	for(auto& x:A)  // for each x in A(x is row here)
 	{
-		for(int j=0;j<3;j++)
+		for(auto& y:x) // for each y in x(y is coloumn here)
 		{
-			C[i][j]=A[i][j]+B[i][j];
-			cout<<C[i][j]<<" ";
+           cin>>y;
+		}
+	
+		
+	}
+	cout<<endl<<"You Entered"<<endl;
+	for(auto& x:A)  // for each x in A(x is row here)
+	{
+		for(auto& y:x) // for each y in x(y is coloumn here)
+		{
+           cout<<y<<" ";
 		}
 		cout<<endl;
 		
