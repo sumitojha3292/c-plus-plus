2d90a0f0219672ca257fa8bf990d5d65d0212c5d
SUMIT <sumitojha3292@gmail.com>
2020-03-25 12:21:31 +0530

    Program to add two 2D array and display result

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-/* CPP program to to print pattern 
-
-* * * * *
-  * * * *
-    * * *
-	  * *
-	    *
-*/ 
+/* CPP program to add two 2D array and print the result*/ 
 
 #include <stdio.h>
 #include <iostream>
@@ -13,17 +6,17 @@ using namespace std;
 
 int main()
 {
-	for(int i=0;i<5;i++)
+int A[2][3]={{2,5,9},{7,3,6}};
+int B[2][3]={{6,3,4},{9,5,2}};
+int C[2][3];
+	for(int i=0;i<2;i++)
 	{
-		for(int j=0;j<5-i;j++)
+		for(int j=0;j<3;j++)
 		{
-			cout<<"*";
+			C[i][j]=A[i][j]+B[i][j];
+			cout<<C[i][j]<<" ";
 		}
 		cout<<endl;
-		for(int j=0;j<=i;j++)
-		{
-		  cout<<" ";	
-		}
 		
 	}
 
