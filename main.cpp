fb83d6d1c8e31ad4727f68c41956cb1a1c706c59
SUMIT <sumitojha3292@gmail.com>
2020-03-26 07:55:39 +0530

    Five Pointer arithmetic cases

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
-/* CPP program for pointers*/ 
+/* CPP program for pointer arithmetic
+pointers support only these five arithmetic operations:-
+int Arr[5]={2,4,6,8,10};
+int *ptr=Arr;
+int *q=&Arr[4];
+1)ptr++;  // pointing to next location
+2)ptr--;  // pointing to previous location 
+3)ptr+=3; // pointing to 3 location next where 3 is any constant value
+4)ptr-=3; // pointing to 3 location previous
+5)n=ptr-q;// ptr and q are seperated by 4 locations, if (n=q-ptr then n=4 that 
+             means that ptr and q are seperated by 4 location and q is farther)
+
+ */ 
 
 #include <stdio.h>
 #include <iostream>
@@ -6,28 +18,27 @@ using namespace std;
 
 int main()
 {
-	int i=0;
-	int *ptr=new int[5];
-	ptr[0]=1;
-	ptr[1]=2;
-	ptr[2]=3;
-	ptr[3]=4;
-	ptr[4]=5;
-    for(i=0;i<5;i++)
-	{
-	 cout<<ptr[i]<<endl;	
-	}
-	
-	delete []ptr;
-	//Again allocate memory for 10 elements
-	cout<<"New memory allocated"<<endl;
-	ptr=new int[10];
-	for(i=0;i<10;i++)
-	{
-	 cout<<ptr[i]<<endl;	
-	}
-	
-	
+	int n, Arr[5]={2,4,6,8,10};
+	int *ptr=Arr,*q=&Arr[4];
+	cout<<ptr<<endl;
+	// step 1
+	ptr++;
+	cout<<ptr<<endl;
+	//step 2
+	ptr--;
+	cout<<ptr<<endl;
+	//step 3
+	ptr+=2;
+	cout<<ptr<<endl;
+	//step 4
+	ptr-=2;
+	cout<<ptr<<endl;
+	// step 5
+	n=ptr-q; // n=-4 which means that q is farther
+	cout<<n<<endl;
+	n=q-ptr;
+	cout<<n<<endl;
+
 
     return 0;
 }
