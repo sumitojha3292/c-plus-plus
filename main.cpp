aafa16bcf213e5cd93a4d2967a320d205bf368d7
SUMIT <sumitojha3292@gmail.com>
2020-03-26 12:54:36 +0530

    program to display two user entered string

--- main.cpp
+++ main.cpp
@@ -1,30 +1,25 @@
-/* Funtion pointer problem for addition and multiplication */ 
+/* CPP for string */ 
 
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
-int add(int a,int b)
-{
-	return a+b;
-}
 
-int mul(int a, int b)
-{
-	return a*b;
-}
 
 int main()
 {
- int result=0;
- int (*fun)(int,int);
- fun=add;
- result=(*fun)(10,5);
- cout<<"Addition= "<<result<<endl;
- fun=mul;
- result=(*fun)(10,5);
- cout<<"Multiplication= "<<result<<endl;
-	
+ char s1[20];
+ char s2[20];
+ 
+ cout<<"Enter 1st name:- "<<endl;
+ cin.get(s1,20);
+ cout<<"Hello "<<s1<<endl;
+ cin.ignore(); // This is added because after giving input, pressing the enter key, enter key taken into 2nd string.
+ cout<<"Enter 2nd name:- "<<endl;
+ cin.get(s2,20);
+ cout<<"Hello "<<s2<<endl;
+ 
+ 
 
     return 0;
 }
