5947421b205e9c27bfd4a7ebd798c5452ad5bae5
SUMIT <sumitojha3292@gmail.com>
2020-03-27 19:40:56 +0530

    Program to separate string token using strtok() api

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
-/* CPP for string */ 
+/* CPP for taking two input string from user */ 
 
 #include <stdio.h>
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 
 
 int main()
 {
- char s1[20];
- char s2[20];
- 
- cout<<"Enter 1st name:- "<<endl;
- cin.get(s1,20);
- cout<<"Hello "<<s1<<endl;
- cin.ignore(); // This is added because after giving input, pressing the enter key, enter key taken into 2nd string.
- cout<<"Enter 2nd name:- "<<endl;
- cin.get(s2,20);
- cout<<"Hello "<<s2<<endl;
- 
- 
-
-    return 0;
+  char S1[]="x=10;y=20;z=30";
+  char *token=strtok(S1,";");
+  while(token!=NULL)
+  {
+	cout<<token<<endl;  
+	token=strtok(NULL,";");// This time NULL is given because already string is passed in previous strtok(); 
+  }
+  
+  
+  return 0;
 }
