45b7a85c5d9e37cd1ec3f6b2089a4e58b4cc0678
SUMIT <sumitojha3292@gmail.com>
2020-03-28 12:58:50 +0530

    Program using string class built-in API

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
-/* CPP for taking two input string from user */ 
+/* CPP for taking string input from user using built in string class API */ 
 
 #include <stdio.h>
 #include <iostream>
-#include <cstring>
+#include <string>
 using namespace std;
 
 
 
 int main()
 {
-  char S1[]="x=10;y=20;z=30";
-  char *token=strtok(S1,";");
-  while(token!=NULL)
-  {
-	cout<<token<<endl;  
-	token=strtok(NULL,";");// This time NULL is given because already string is passed in previous strtok(); 
-  }
-  
-  
+ string str1;
+ cout<<"Enter the string"<<endl; 
+ getline(cin,str1);
+ cout<<str1<<endl;
+ cout<<"Length= "<<str1.length()<<endl;  // for calculating string length
+ cout<<"Capacity= "<<str1.capacity()<<endl; // for calculating string length
+ str1.resize(50);                          // it will make sure that new size will be >=50
+ cout<<"New capacity= "<<str1.capacity()<<endl; 
+ cout<<"string maxm size= "<<str1.max_size()<<endl; // for checking maxm string supported size
+// str1.clear(); // Clear the contents of string
+ if(str1.empty())
+ {
+	 cout<<"Empty string"<<endl;
+	
+ }
+ else
+ {
+	  cout<<"string is "<<str1<<endl;
+ }
+ 
   return 0;
 }
