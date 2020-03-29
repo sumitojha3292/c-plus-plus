853be14d2e50e1d8c2ab31c73bae0b97897f3986
SUMIT <sumitojha3292@gmail.com>
2020-03-29 20:16:51 +0530

    CPP program for Function overloading

--- main.cpp
+++ main.cpp
@@ -1,31 +1,38 @@
-/* CPP to traverse,reverse,changing upper-lower and lower to upper case using a string iterator */ 
+/* CPP for function overloading
+ A function with same name can be redifened but its argument sould be different.
+ If a function with same name and argument list but different return type is not 
+ a function overloading.
+*/ 
 
 #include <stdio.h>
 #include <iostream>
 #include <string>
 using namespace std;
 
+int add(int a,int b)
+{
+	return(a+b);
+}
 
+int add(int a, int b, int c)
+{
+	return(a+b+c);
+}
+
+float add(float a, float b)
+{
+	return(a+b);
+}
 
 int main()
 {
-  string s1="welcome";
-  string s2="HELLO";
-  string::iterator it;
-  string::reverse_iterator rit;
-  // traversing through string and changing it to upper case
-  for(it=s1.begin();it!=s1.end();it++)
-  {
-	  *it=*it-32;
-  }
-  cout<<s1<<endl;
-
-  
-  // Reversing a string and changing it to lower case
- for(rit=s2.rbegin();rit!=s2.rend();rit++)
-  {
-	  *rit=*rit+32;
-  }
-   cout<<s2<<endl;
+  int sum=0;	
+  sum=add(10,20);
+  cout<<"sum_1= "<<sum<<endl;
+  sum=add(10,20,30);  
+  cout<<"sum_2= "<<sum<<endl;
+  sum=add(10.5f,20.5f);
+  cout<<"sum_3= "<<sum<<endl;
+	
   return 0;
 }
