d9d8b2fd62ed12cc3277845d26335c707bd909d8
SUMIT <sumitojha3292@gmail.com>
2020-03-31 23:19:49 +0530

    Program  for data hiding in class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* CPP for class using pointer and allocate memory from heap*/ 
+/* CPP for class data hiding*/ 
 
 #include <stdio.h>
 #include <iostream>
@@ -7,9 +7,32 @@ using namespace std;
 
 class rectangle
 {
-	public:
+    private:
 	int length;
 	int breadth;
+	public:
+	void set_data(int l,int b)
+	{
+		if(l>=0)
+			length=l;
+		else
+			length=0;
+		
+		if(b>=0)
+			breadth=b;
+		else
+			breadth=0;
+	}
+	
+	int get_length(void)
+	{
+		return length;
+	}
+	
+	int get_breadth(void)
+	{
+		return breadth;
+	}
 	
 	int area()
 	{
@@ -24,10 +47,9 @@ class rectangle
 
 int main()
 {
-	rectangle *R1= new rectangle;  // Memory is allocated from heap
-	R1->length=10;
-	R1->breadth=20;
-	cout<<"Area= "<<R1->area()<<endl<<"Perimeter= "<<R1->perimeter()<<endl;
+	rectangle R1;
+    R1.set_data(15,4);
+	cout<<"Area= "<<R1.area()<<endl<<"Perimeter= "<<R1.perimeter()<<endl;
 
   return 0;
   
