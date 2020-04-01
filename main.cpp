927aa3dada60d047bbc82c8060a960ad2e842c1c
SUMIT <sumitojha3292@gmail.com>
2020-04-01 20:09:24 +0530

    program to use constructor in a class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-/* CPP for class data hiding*/ 
+/* CPP for using constructor in a class
+Constructor is defined as the same name as of class.There are mainly three kind of constructor:-
+1) Non parameterized
+2) Parameterized
+3) Copy 
+
+ */ 
 
 #include <stdio.h>
 #include <iostream>
@@ -11,6 +17,29 @@ class rectangle
 	int length;
 	int breadth;
 	public:
+	/******* Constructors****************/
+	// 1-Non parameterized
+     rectangle()
+	 {
+		 length=0;
+		 breadth=0;
+	 }
+
+
+    // 2-Parameterized
+	rectangle(int l,int b)   // we can also use parameterized and non parameterized constructor                           
+	{                        // as a funcn overloading by initializing the arguments with zero.
+		set_data(l,b);
+	}
+	
+	// 3-Copy constructor
+	rectangle(rectangle &r)
+	{
+		length=r.length;
+		breadth=r.breadth;
+	}
+  
+
 	void set_data(int l,int b)
 	{
 		if(l>=0)
@@ -47,9 +76,13 @@ class rectangle
 
 int main()
 {
-	rectangle R1;
-    R1.set_data(15,4);
-	cout<<"Area= "<<R1.area()<<endl<<"Perimeter= "<<R1.perimeter()<<endl;
+	rectangle R1(5,12); // parameterized constructor
+	rectangle R2(R1);   // copy constructor
+	rectangle R3;       // Non Parameterized constructor
+	cout<<"Area of R1= "<<R1.area()<<endl<<"Perimeter of R1= "<<R1.perimeter()<<endl;
+	cout<<"Area of R2= "<<R2.area()<<endl<<"Perimeter of R2= "<<R2.perimeter()<<endl;
+	cout<<"Area of R3= "<<R3.area()<<endl<<"Perimeter of R3= "<<R3.perimeter()<<endl;
+
 
   return 0;
   
