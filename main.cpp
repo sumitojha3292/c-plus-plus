2c38c09d0dae5d30ff62b4af6bfdff253d2e7b0a
SUMIT <sumitojha3292@gmail.com>
2020-04-02 23:23:35 +0530

    Program to use operator overloading

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-/* CPP for defining structure 
+/* CPP for defining Operator overloading. 
+  we can use several operator like (+,-,*,(),++,--,new,delete).
  */ 
 
 #include <stdio.h>
@@ -6,35 +7,39 @@
 
 using namespace std;
 
-struct rectangle
+struct Complex
 {
-	int length;
-	int breadth;
-	
-	int area()
-	{
-		return (length*breadth);
-	}
-	
-	int perimeter()
-	{
-		return (2*(length+breadth));
-	}
-     
-	rectangle(int l,int b)
-	{
-		length=l;
-		breadth=b;
-	}	
+ public:
+ int real;
+ int img;
+
+ Complex operator+(Complex C)
+ {
+	 Complex temp;
+	 temp.real=real+C.real;
+	 temp.img=img+C.img;
+	 return temp;
+ } 
+ 
+ Complex operator-(Complex C)
+ {
+	 Complex temp;
+	 temp.real=abs(real-C.real);
+	 temp.img=abs(img-C.img);
+	 return temp;
+ }
 
 };
 
 int main()
 {
-  rectangle R1(5,10);
-  cout<<"Area= "<<R1.area()<<endl;
-  cout<<"Perimeter= "<<R1.perimeter()<<endl;  
-
+ Complex C1,C2,C3,C4;
+ C1.real=10;C1.img=5;
+ C2.real=4;C2.img=8; 
+ C3=C1+C2;
+ C4=C1-C2;
+ cout<<"C3= "<<C3.real<<" +i"<<C3.img<<endl;
+ cout<<"C4= "<<C4.real<<" +i"<<C4.img<<endl;
 	
   return 0;
   
