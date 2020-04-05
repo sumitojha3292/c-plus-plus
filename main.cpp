f4bc1361c427d5f6bc442e55c09b649e6bb3b153
SUMIT <sumitojha3292@gmail.com>
2020-04-05 22:52:44 +0530

    Parent class pointer is address to child class object

--- main.cpp
+++ main.cpp
@@ -1,30 +1,40 @@
-/* Funtion pointer problem for addition and multiplication */ 
+/*CPP for parent class pointer and child object.
+child class is always inherited from parent class so (parent-->child)
+pointer of parent class can be addressed to object of child class.
+*/ 
 
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
-int add(int a,int b)
-{
-	return a+b;
-}
 
-int mul(int a, int b)
+class parent
 {
-	return a*b;
-}
+	public:
+	void func_parent()
+	{
+		cout<<"parent class"<<endl;
+	}
+};
+
+class child:public parent
+{
+	public:
+	void func_child()
+	{
+		cout<<"child class"<<endl;
+	}
+};
+
 
 int main()
 {
- int result=0;
- int (*fun)(int,int);
- fun=add;
- result=(*fun)(10,5);
- cout<<"Addition= "<<result<<endl;
- fun=mul;
- result=(*fun)(10,5);
- cout<<"Multiplication= "<<result<<endl;
-	
+	child C;
+	C.func_child();
+	C.func_parent();
+	parent *p=&C;
+	p->func_parent();  
+   // p->func_child();   //	This will give error that parent class has no member as func_child().
 
     return 0;
 }
