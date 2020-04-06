8afb05a184817d740e927dc9440509adb9f03936
SUMIT <sumitojha3292@gmail.com>
2020-04-06 13:04:06 +0530

    program for polymorphism using virtual function overriding technique

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-/*CPP for function overriding method in polymorphism.
-In function overriding method same function signature is defined in parent and child class. 
+/*CPP for polymorphism using virtual function overiding.
+A virtual function is a special type of function that, when called, resolves 
+to the most-derived version of the function that exists between the base 
+and derived class. This capability is known as polymorphism.
+when same instruction is executing different results, it is known as polymorphism.
+
 */ 
 
 #include <stdio.h>
@@ -10,27 +14,45 @@ using namespace std;
 class parent
 {
 	public:
-	void display()
+	virtual void display()
 	{
 		cout<<"this is parent class"<<endl;
 	}
 };
 
-class child:public parent
+class child_1:public parent
 {
 	public:
 	void display()        // this is function overiding
 	{
-		cout<<"this is child class"<<endl;
+		cout<<"this is child_1 class"<<endl;
 	}
 };
 
 
+class child_2:public parent
+{
+	public:
+	void display()        // this is function overiding
+	{
+		cout<<"this is child_2 class"<<endl;
+	}
+};
+
+/* ptr->display() is used twice in main function but each time 
+giving different result is known as polymorphism */
+
 int main()
 {
-	child c;
-	c.display();  // child class display function will be called
-	c.parent::display(); // parent class display function will be called 
+	child_1 c1;
+	child_2 c2;
+	parent *ptr=&c1;
+	c1.parent::display();
+	ptr->display(); 
+	ptr=&c2;
+	ptr->display();
+	c2.parent::display();
+
     
     return 0;
 }
