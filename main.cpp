1e1426ad522c3bca79a4e67f7d2a7dea890f2248
SUMIT <sumitojha3292@gmail.com>
2020-04-07 11:16:16 +0530

    Accessing private member of class using friend operator

--- main.cpp
+++ main.cpp
@@ -1,44 +1,64 @@
-/* CPP for defining structure 
- */ 
+/*CPP for accessing private,protected and public data member on object by using friend function.*/ 
 
 #include <stdio.h>
 #include <iostream>
-
 using namespace std;
 
-struct rectangle
+class child;    // defining a class prototype
+
+
+class parent
 {
-	int length;
-	int breadth;
+	private:
+	int a;
+	protected:
+	int b;	
+	public:
+	int c;
+	friend void func();    // declaring a friend function
+	friend class child;   // declaring a friend class
 	
-	int area()
-	{
-		return (length*breadth);
-	}
 	
-	int perimeter()
-	{
-		return (2*(length+breadth));
-	}
-     
-	rectangle(int l,int b)
-	{
-		length=l;
-		breadth=b;
-	}	
 
 };
 
-int main()
-{
-  rectangle R1(5,10);
-  cout<<"Area= "<<R1.area()<<endl;
-  cout<<"Perimeter= "<<R1.perimeter()<<endl;  
 
+void func()
+{
+	parent p;        // we can only acess the private member upon object.we can't acess the private member directly.
+	p.a=1;
+	p.b=2;
+	p.c=3;
+	cout<<"In func:"<<endl;
+	cout<<"a= "<<p.a<<endl<<"b= "<<p.b<<endl<<"c= "<<p.c<<endl;
 	
-  return 0;
-  
 }
 
+class child
+{
+	public:
+	parent p1;
+	
+	void display()
+	{
+		p1.a=10;    // we can only acess the private member upon object.we can't acess the private member directly.
+		p1.b=20;
+		p1.c=30;
+		cout<<"In class child:"<<endl;
+		cout<<"a= "<<p1.a<<endl<<"b= "<<p1.b<<endl<<"c= "<<p1.c<<endl;
+		
+		
+		
+	}
+};
 
 
+
+
+int main()
+{ 
+    child c;
+	c.display();
+    func();
+    return 0;
+}
