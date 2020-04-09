d392082fad91322c0c9202ebae17b6bf73b0af0e
SUMIT <sumitojha3292@gmail.com>
2020-04-09 20:50:42 +0530

    Program for template

--- main.cpp
+++ main.cpp
@@ -1,81 +1,56 @@
-/*CPP for construct of exception handling using try and catch block. 
+/*CPP for using template . 
+1-Templates are functions that serve as a pattern for creating other similar functions.
+2-The basic idea behind function templates is to create a function without having to specify
+  the exact type of the variables.
+3-To create a template type parameter, use either the keyword typename or class.There is no 
+  difference between the two keywords in this context, so which you use is up to you.Note that
+  if you use the class keyword, the type passed in does not actually have to be a class 
+  (it can be a fundamental variable, pointer, or anything else that matches).
+4-Template function uses multiple template type parameter, they can be separated by commas.
+5- Whenever a new function is starts it should be defined with template. template declaration effect 
+   is till the immidiate function,class body ends.
 */ 
 
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
-class stackoverflow:public exception{};
-class stackunderflow:public exception{};
-
-class stack
+template<class T>
+class Test
 {
-	private:
-	int *ptr;
-	int top=-1;
-	int size;
-	
-	public:
-	stack(int sz)
-	{
-		if(sz>0)
-		{
-			size=sz;
-			ptr=new int[size];
-		}
-		else
-		{
-			throw string("invalid size");
-		}
-		
-	}
-	
-	void push(int x)
-	{
-		if(top==size-1)
-		{
-			throw stackoverflow();
-		}
-		top++;
-		ptr[top]=x;
-	}
-	
-	int pop()
-	{
-		if(top==-1)
-		{
-			throw stackunderflow();
-		}
-		return ptr[top--];
-	}
+  public:
+  T maxim(T x,T y)
+  {
+	  return x>y?x:y;
+  }
+  
+  T minm(T x,T y)
+  {
+	  return x<y?x:y;
+  }
+  
+  T add(T x,T y)
+  {
+	  return x+y;
+  }
+  
+  T substract(T x,T y)
+  {
+	  return x-y;
+  }
 };
 
 
 
+
+
 int main()
-{ 
-   stack s(5);
-   try
-   {
-	   s.push(5);
-	   s.push(6);
-	   s.push(7);
-	   s.push(8);
-	   s.push(9);
-	   s.push(1);
-   }
-   catch(string s)
-   {
-	   cout<<s<<endl;
-   }
-   catch(class stackoverflow)
-   {
-	   cout<<"stackoverflow"<<endl;
-   }
-   catch(class stackunderflow)
-   {
-	   cout<<"stackunderflow"<<endl;
-   }
+{
+	
+   Test<int> t;
+   cout<<"maximum= "<<t.maxim(10,20)<<endl;
+   cout<<"sum= "<<t.add(5,6)<<endl;
+
 
     return 0;
 }
