3fb8ec469d8b42a503e6053c95d46b5d5a8fafcd
SUMIT <sumitojha3292@gmail.com>
2020-04-09 08:23:24 +0530

    program for exception handling using try and catch block

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-/*CPP for static members and static functions of a class.
-1- Object of the inner class should be created after defination of inner class.
-2- Inner class can only access the static members of the outer class. 
-3- Outer class can access all the public members of the inner class(it can't access private and protected members).
-4- we can also create the object of inner class outside outer class using scope resolution.
-5- if we don't want inner class to visible outside outer class then we can define inner class as private.
+/*CPP for construct of exception handling using try and catch block.
+Exceptions in C++ are implemented using three keywords that work in conjunction with 
+each other: throw, try, and catch.
+1- Throw statement is used to signal that an exception or error case has occurred.
+2- The try block acts as an observer, looking for any exceptions that are thrown by 
+   any of the statements within the try block. 
+3- The catch keyword is used to define a block of code (called a catch block) that 
+   handles exceptions for a single data type.
+4- Try blocks and catch blocks work together.A try block detects any exceptions that are 
+   thrown by statements within the try block, and routes them to the appropriate catch block for handling.
+5- A try block must have at least one catch block immediately following it, but may have multiple catch 
+   blocks listed in sequence.
+6- Try and Catch block is used mostly when we are calling a function.   
+7- We can throw any thing like integer value,double,float,string,enums,class etc.
+8- We can have a catch block which can handle any type of throw is called ellipse(all-catch).
+   this catch block must be the last block.
+9- catch block is only executed when something is thrown.
+10- We can do the nesting of try-catch block.
+11- Catch block of child class should be above of parent class. it should always maintain the hierarchy. 
 */ 
 
 #include <stdio.h>
@@ -11,58 +24,77 @@
 using namespace std;
 
 
-class outer
+class my_exception:public exception        // there is also built in class in c++ for exception handling. we can also use it
 {
-	private:
+  public:
+  int a;
 	
-	int a;
-	static int b;
-	
-	public:
-	//inner i;  // we can't create inner class object before defination of its class(Rule-1)
-	void func()
-	{
-		cout<<"a= "<<a<<endl<<"b= "<<b<<endl;
-		i.show();
-	}
-	
-	class inner
-	{
-		private:
-		 int c=20;
-		
-		protected:
-		int d=30;
-		
-		public:
-
-		void show()
-		{
-			cout<<"This is inner class:"<<endl;
-			//cout<<"a= "<<a<<endl; // Rule-2
-			b=10;        // Rule-2
-		}
-	};
-	
-	inner i; // Rule-1
-	
-
 };
 
-int outer::b=0;   // static variables should also be define globally.
-
-
-
+void check_exception(int x)
+{
+	if(x==0)
+	{
+		throw 1;   // throwing integer
+	}
+	else if(x==1)
+	{
+		throw 1.5; // throwing double
+	}
+	else if(x==2)
+	{
+		throw 1.5f;  // throwing float
+	}
+	else if(x==3)
+	{
+		throw string("sumit"); // throwing string
+	}
+	else if(x==4)
+	{
+		throw my_exception(); // throwing class
+	}
+	else
+	{
+		
+	}
+	
+}
 
 
 
 int main()
 { 
-   outer::inner t;    // Rule-4
-   t.show();         // Rule-3
-   outer o;
-   o.func();
-   //cout<<"c= "<<o.c<<endl<<"d= "<<o.d<<endl; // Rule-3
+    int a=0,cnt=5;
+	while(cnt)
+	{
+		try
+		{
+		  check_exception(a);	
+		}
+		catch(int x)
+		{
+			cout<<"integer catch"<<endl;
+		}
+		catch(double x)
+		{
+			cout<<"double catch"<<endl;
+		}
+		catch(float x)
+		{
+			cout<<"float catch"<<endl;
+		}
+		catch(string x)
+		{
+			cout<<"string catch"<<endl;
+		}
+		catch(...)
+		{
+			cout<<"All catch"<<endl;
+		}
+		cnt--;
+		a++;
+	}
+
 
     return 0;
 }
