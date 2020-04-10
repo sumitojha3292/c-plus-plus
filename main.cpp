533e3e3db2f9ae22aa5bf025eb43e81933b6cb33
SUMIT <sumitojha3292@gmail.com>
2020-04-10 16:35:06 +0530

    program for using const qualifier in a class

--- main.cpp
+++ main.cpp
@@ -1,68 +1,35 @@
-/*CPP for static members and static functions of a class.
-1- Object of the inner class should be created after defination of inner class.
-2- Inner class can only access the static members of the outer class. 
-3- Outer class can access all the public members of the inner class(it can't access private and protected members).
-4- we can also create the object of inner class outside outer class using scope resolution.
-5- if we don't want inner class to visible outside outer class then we can define inner class as private.
-*/ 
+/*CPP for using const qualifier in a class.*/ 
 
 #include <stdio.h>
 #include <iostream>
 using namespace std;
 
-
-class outer
+class demo
 {
-	private:
-	
-	int a;
-	static int b;
-	
 	public:
-	//inner i;  // we can't create inner class object before defination of its class(Rule-1)
-	void func()
+	int x=10;
+	int y=20;
+	
+	void display()const  // defining a function as a constant
 	{
-		cout<<"a= "<<a<<endl<<"b= "<<b<<endl;
-		i.show();
+		//x++; // it will give error 
+		//y++; // it will give error 
+		cout<<"display function"<<endl<<"value of x= "<<x<<endl<<"value of y= "<<y<<endl;
 	}
 	
-	class inner
+   void increment()
 	{
-		private:
-		 int c=20;
-		
-		protected:
-		int d=30;
-		
-		public:
-
-		void show()
-		{
-			cout<<"This is inner class:"<<endl;
-			//cout<<"a= "<<a<<endl; // Rule-2
-			b=10;        // Rule-2
-		}
-	};
-	
-	inner i; // Rule-1
-	
-
+		x++;
+		y++;
+		cout<<"increment func"<<endl<<"value of x= "<<x<<endl<<"value of y= "<<y<<endl;
+	}
 };
-
-int outer::b=0;   // static variables should also be define globally.
-
-
-
-
-
 
 int main()
 { 
-   outer::inner t;    // Rule-4
-   t.show();         // Rule-3
-   outer o;
-   o.func();
-   //cout<<"c= "<<o.c<<endl<<"d= "<<o.d<<endl; // Rule-3
+    demo d;
+	d.display();
+	d.increment();
 
     return 0;
 }
