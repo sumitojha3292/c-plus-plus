9641425f1dfa0e8d4228d2f480970f41b58323b9
SUMIT <sumitojha3292@gmail.com>
2020-04-13 12:14:07 +0530

    c++11 features

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-/*CPP for Manupulators.
-1- Manipulators are helping functions that can modify the input/output stream. some examples are:-
-   hex,dec,oct,fixed,scientific etc.
-2- For more information go to url:- https://www.geeksforgeeks.org/manipulators-in-c-with-examples/
+/*CPP for c++11 features.
+1- Auto keyword is used to declare data type and return type automatically.
+2- decltype(x) y is used to declare y as same type as of x. 
+3- Final keywords in class declaration restricts inheritance. final keyword is used only in virtual function
+   to avoid function overriding in child class. 
 */             
 
 #include <stdio.h>
@@ -9,18 +10,35 @@
 #include <fstream>  // Including for file input output stream
 using namespace std;
 
+class parent //final     // if uncomment it will generate error bcz child class is derived from parent class.
+{
+	public:
+	//virtual void show() final  // if uncomment, it will generate error bcz function can't be override from\
+                                	virtual function with final keyword. 
+	void show()
+	{
+		cout<<"parent class"<<endl;
+	}
+};
 
+class child:parent
+{
+	public:
+	void show()
+	{
+		cout<<"child class"<<endl;
+	}
+};
 
 int main()
 { 
-  cout<<hex<<60<<endl; 
-  cout<<dec<<0x3c<<endl; 
-  cout<<dec<<0b010101<<endl;
+  auto x=5.2; // x will automatically become double type
+  auto sum=0.0;
+  decltype(x) y=2.3;  // y is of type x
+  sum=x+y;
+  cout<<"sum= "<<sum<<endl;
+  child c;
+  c.show();
   
-  cout<<fixed<<12.195<<endl;        // used for showing fixed floating value
-  cout<<scientific<<12.195<<endl;  // scientific value in exponent and mantisa  
-  
-
-
-    return 0;
+  return 0;
 }
