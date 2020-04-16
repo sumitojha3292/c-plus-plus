0269989986d80e1eb2f94ac297f6523d7ddd81d6
SUMIT <sumitojha3292@gmail.com>
2020-04-16 20:39:11 +0530

    Returning multiple values from a funtion using class

--- main.cpp
+++ main.cpp
@@ -1,50 +1,52 @@
-/*CPP for using virtual Destructor in a parent and child class.
-1- Destructor is a function which is called automatically when an object is destroyed.
-2- When you create an object in a heap then only constructor is called. for calling a destructor 
-   object should be deleted using delete operator.
-3- Unlike constructor, destructor of child class is called first than parent class.so destructor
-   is called from bottom to top.   
-*/ 
+/*CPP for returning multiple values from function using class.
+In this program maximum,minm,avg value from array is returned from a function */             
 
 #include <stdio.h>
 #include <iostream>
+#include <cstdarg> // needed to use ellipsis
 using namespace std;
 
-
-class parent
+class Mul_val
 {
 	public:
-    parent()
-	{
-		cout<<"parent-constructor"<<endl;
-	}
+	int max;
+	int minm;
+	float avg;
 	
-	virtual ~parent()
-	{
-		cout<<"parent-destructor"<<endl;
-	}
-
 };
 
-class child: public parent
+Mul_val test_fucn(int *arr,int size)
 {
-	public:
-    child()
+	int i=0;
+	Mul_val C;
+	C.max=arr[0];
+	C.minm=arr[0];
+	C.avg=0;
+	for(i=0;i<size;i++)
 	{
-		cout<<"child-constructor"<<endl;
+		if(C.max>arr[i])
+		{
+			C.max=arr[i];
+		}
+		if(C.minm<arr[i])
+		{
+			C.minm=arr[i];
+		}
+		C.avg+=arr[i];
+		
 	}
-	
-	~child()
-	{
-		cout<<"child-destructor"<<endl;
-	}
-
-};
+	C.avg/=size;
+	return C;
+}
 
 int main()
-{  
-   parent *ptr=new child();// constructor is called from top to bottom that is from parent class to child class.
-   delete ptr; // if virtual destructor is not used then only parent class destructor will be called bcz pointer\
-                 is of type parent class. for making child class destructor also to be called, virtual destructor is used. 
-    return 0;
+{ 
+  int arr[5]={2,5,3,7,1};
+  Mul_val M;
+  M=test_fucn(arr,5);
+  cout<<"Maximum= "<<M.max<<endl;
+  cout<<"Minimum= "<<M.minm<<endl;
+  cout<<"Average= "<<M.avg<<endl;
+ 
+  return 0;
 }
