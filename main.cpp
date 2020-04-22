2b4bdf60bd10d4484757c2d06b6981474cd45bb8
SUMIT <sumitojha3292@gmail.com>
2020-04-22 21:03:06 +0530

    Returning multiple values from function using tuple

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-/*CPP for using STL tuple class. 
-1-Tuple provides a way to store multiple heterogeneous objects as a single unit.
-  syntax:- tuple<T1,T2,T3..,Tn> Tuple1; where T1 to Tn can be data types,class etc.
-           Tuple1=make_tuple(T1,T2,..Tn) for inserting values in a pair.
-		   For acessing members of tuple use get<0>(Tuple1) for acessing first member of tuple
-		   and get<n>(Tuple1) for nth member of Tuple1 .
-
-*/ 
+/*CPP for returning multiple values from function using tuple*/ 
 
 #include <stdio.h>
 #include <iostream>
@@ -14,46 +7,21 @@
 
 using namespace std;
 
-class student
+tuple<string,int,int> info(void)
 {
-	private:
-	string name;
-	int marks;
-	
-	public:
-	student()
-	{
-		cout<<"Student Info:"<<endl;
-		
-	}
-	
-	student(string s, int m)
-	{
-		name=s;
-		marks=m;
-	}
-	
-	void show(void)
-	{
-		cout<<"name- "<<name<<"  "<<"marks: "<<marks<<endl;
-	}
-	
-};
+	return make_tuple("Sumit",28,85);
+}
 
 int main()
 {
 	/*****defining a Tuple****************/
-	tuple<student,string,int,string,string> t1; 
-	student s1("Sumit",85);
-	/*****Inserting a Value*************/
-	t1=make_tuple(s1,"Age:",28,"Hobby:","cricket");
+     tuple<string,int,int> t1;
+	 t1=info();
 	
 	/**********Display*****************/
-	s1.show();
-	cout<<get<1>(t1)<<endl;	
-	cout<<get<2>(t1)<<endl;
-	cout<<get<3>(t1)<<endl;
-	cout<<get<4>(t1)<<endl;
+     cout<<"name of student: "<<get<0>(t1)<<endl;
+	 cout<<"age: "<<get<1>(t1)<<endl;
+	 cout<<"marks: "<<get<2>(t1)<<endl;
 	
 
    return 0;
